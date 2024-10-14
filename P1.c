86a862de4f7e38421b46df8043c06c22f2dad3db
Bill Kent <billkent289@gmail.com>
2024-10-14 22:17:45 +0700

    Create P1.c

---
+++ P1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float alas, tinggi, luas;
+
+    printf("Masukkan alas segitiga: ");
+    scanf("%f", &alas);
+
+    printf("Masukkan tinggi segitiga: ");
+    scanf("%f", &tinggi);
+
+    luas = (alas * tinggi) / 2;
+
+    printf("Luas segitiga adalah: %.2f\n", luas);
+
+    return 0;
+}
